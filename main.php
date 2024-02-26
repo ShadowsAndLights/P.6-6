86ab55d26cd95b3ca70a5bb08ca91bdf5f94c79f
ShadowsAndLights <optimistic1988@live.fr>
2024-02-26 15:29:51 +0100

    Ajout du fichier PHP pour functions

---
+++ main.php
@@ -0,0 +1,56 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+         
+if ( !function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css() {
+        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'hello-elementor','hello-elementor','hello-elementor-theme-style','hello-elementor-header-footer' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+
+// END ENQUEUE PARENT ACTION
+
+?>
+
+
+<?php
+
+// HOOK LIEN ADMIN 
+
+
+function add_adminlink($items, $args){
+    if (is_user_logged_in()) {
+        $items .='<li id="AdminLink"><a href="'.get_admin_url().'">Admin</a></li>';
+    }
+
+    return $items;
+    }
+ 
+
+add_filter('wp_nav_menu_items','add_adminlink', 10, 2);
+
+?>
+
+
+
+
+
+
+
+
+
+
+         
